0e3aaef21de75aa456ebf704c34ef90d84c11406
Minkwan Kim <minkwan.kim@adelaide.edu.au>
2015-05-11 14:55:52 +0930

    Initial Test

---
+++ OP2A/Common/include/Standard_headers.hpp
@@ -0,0 +1,32 @@
+/*
+ * Open-source multi-Physics Phenomena Analyzer (OP2A) ver. 0.1
+ *
+ * 		Copyright (c) 2015 MINKWAN KIM
+ *
+ * 	Initial Developed Date: May 11, 2015
+ *      			Author: Minkwan Kim
+ *
+ * Standard_headers.hpp
+ * 			-  
+ *  
+ */
+#ifndef STANDARD_HEADERS_HPP_
+#define STANDARD_HEADERS_HPP_
+
+
+
+#include <memory>    	// std::auto_ptr
+#include <string>    	// std::string
+#include <sstream>  	// string stream
+#include <vector>    	// std::vector
+#include <map>       	// std::map
+#include <cmath>     	// all sorts of mathematical functions and constants
+#include <algorithm> 	// all sorts of algorithms on stl containers
+#include <utility>   	// stl::pair
+#include <typeinfo>  	// for typeid
+#include <complex>   	// for complex numbers and complex functions
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+
+#endif /* STANDARD_HEADERS_HPP_ */
